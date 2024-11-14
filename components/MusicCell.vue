c23a180dd4305f495fa57c75c98e848a9607f6c9
rorikoron <sub.ac.000000001@gmail.com>
2024-11-15 02:20:23 +0800

    adjusted format of audio length

--- components/MusicCell.vue
+++ components/MusicCell.vue
@@ -5,11 +5,30 @@ const props = defineProps<{
     music: musicProps,
     idx: number
 }>();
+
+const computedAudioLength = computed(() => {
+    let string = '';
+    let totalSec = Math.round(props.music.audioLength);
+    if(Math.floor(totalSec) / 3600 >= 1){
+        string += (Math.floor(totalSec / 3600) + ':');
+        totalSec -= Math.floor(totalSec / 3600) * 3600;
+    } 
+    if(Math.floor(totalSec) / 60 >= 1){
+        string += (Math.floor(totalSec / 60) + ':');
+        totalSec -= Math.floor(totalSec / 60) * 60;
+    } 
+    if(totalSec < 10){
+        string += '0';
+    }
+    string += totalSec;
+
+    return string
+})
 </script>
 
 <template>
     <span>{{ props.idx }}</span>
     <span>{{ props.music.title }}</span>
     <span>{{ props.music.lastUpdate }}</span>
-    <span>{{ props.music.audioLength }}</span>
+    <span>{{ computedAudioLength }}</span>
 </template>
